dee6b20b1fb0efa38d4346a7b006beee96dcbc63
JamesHoi <james352053767@gmail.com>
2020-02-21 17:57:47 +0800

    feat: add real-time stream

--- speech/rt_stream.py
+++ speech/rt_stream.py
@@ -1,94 +1,15 @@
-import os
 import time
-import wave  # 调用wave模块
-import numpy as np  # 调用numpy模块记作np
-from misc import *
-from const import *
+from speech.sound_cal import *
 
-min_amplitude = 0
-is_talking,tic,toc = False,False,False
-tic_stop = 0
-
-#如果秒数等于0，则获取最短语音数据
-def get_recordFrames(seconds):
-    #print("* recording")
-    # 打开数据流
-    stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
-    # 开始录音
-    frames = []
-    for i in range(0, int(RATE / CHUNK * seconds)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-    # 关闭数据流
-    stream.stop_stream()
-    stream.close()
-    #print("* done recording")
-    return b''.join(frames)
-
-# 保存语音为.pcm
-def save_recordFrames(filename,frames):
-    wf = wave.open(filename, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(frames)
-    wf.close()
-
-# 是否标定
-def iscalibrated():
-    return os.path.exists(WAVE_OUTPUT_FILENAME)
-
-# 校正时的录音长度
-# 灵敏度
-# 返回判断为有声音的最小振幅
-def calibrate(sensitivity):
-    # 获取语音
-    if(os.path.isfile(WAVE_OUTPUT_FILENAME)):
-        print("找到標定音頻")
-        f = wave.open(WAVE_OUTPUT_FILENAME, "rb")  # 读取语音文件
-        nchannels, sampwidth, framerate, nframes = f.getparams()[:4]  # 获取音频参数
-        wave_data = f.readframes(nframes)
-        global RECORD_SECONDS,RATE
-        RATE = framerate
-        RECORD_SECONDS = nframes/framerate
-    else:
-        wave_data = get_recordFrames(RECORD_SECONDS)
-    # 转换语音数据格式
-    wave_data = np.fromstring(wave_data, dtype=np.short)
-    # 记录语音，每次打开只需输入灵敏度
-    save_recordFrames(WAVE_OUTPUT_FILENAME, wave_data)
-    # 归一化
-    wave_data_normal = wave_data * 1.0 / (max(abs(wave_data)))
-    #灵敏度判断
-    filter = np.where(wave_data_normal >= 1-sensitivity)
-    if len(filter[0]) != 0:
-        # 找到最小值的索引
-        min_index = filter[0][np.argmin(wave_data[filter])]
-        # 半秒的采样点数
-        half_seconds_frames = RATE*0.5
-        # 取灵敏度之中最小值的半秒内
-        if(min_index-half_seconds_frames/2<0):
-            half = wave_data[0:int(min_index+half_seconds_frames)]
-        elif(min_index+half_seconds_frames/2>wave_data.size):
-            half = wave_data[int(wave_data.size-half_seconds_frames):wave_data.size]
-        else:
-            half = wave_data[int(min_index-half_seconds_frames/2):int(min_index+half_seconds_frames/2)]
-        # 均值
-        global min_amplitude
-        min_amplitude = wave_data[min_index]#np.mean(np.abs(half))/2
-        return min_amplitude
-    else:
-        print("靈敏度太低，檢測不到你正在說話，退出程序重試")
-        return 0
-
-# 获取振幅
-def getAmplitude(frames):
-    if(len(frames) < MIN_BUFFER*CHUNK):
-        return 0
-    else:
-        amplitude = np.max(np.abs(np.fromstring(frames, dtype=np.short)))
-        if DEBUG:print(amplitude)
-        return amplitude
+# 识别语音的参数
+# 说话最大间断秒数
+MAX_INTERVAL_TIME = 0.5
+#百度API超时时间60秒
+TIMEOUT_API = 58
+#语音前后留多少空闲时间
+LEISURE_TIME = 0.5
+# 说话多久超时
+TIMEOUT_USER = TIMEOUT_API
 
 # 判断是否停止说话，并且在语音内添加前后空余时间
 def checktalk(_is_talking):
@@ -120,6 +41,7 @@ def checktalk(_is_talking):
         return False
 
 def get_talkOnce():
+    p = pyaudio.PyAudio()
     global is_talking
     samp = []
     frames = []
@@ -147,9 +69,6 @@ def get_talkOnce():
         if (not checktalk(_is_talking) and amplitude != 0):
             return b''.join(samp+frames)
     return b''.join(samp+frames)
-
-def quit():
-    p.terminate()
 
 def input_loop(input_str):
     def isscalar(str):
---
+++ speech/stream.py
@@ -0,0 +1,35 @@
+import pyaudio
+import wave
+
+# 录音参数
+# 定义数据流块
+CHUNK = 2000#1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1#2
+RATE = 16000
+
+# 调试
+DEBUG = False
+
+def get_recordFrames(seconds):
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+    # 开始录音
+    frames = []
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    # 关闭数据流
+    stream.stop_stream()
+    stream.close()
+    #print("* done recording")
+    return b''.join(frames)
+
+# 保存语音为.pcm
+def save_recordFrames(filename,frames):
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(frames)
+    wf.close()
