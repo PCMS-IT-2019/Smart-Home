fee4139798a148a69db014cae9533fdd64bca2a6
JamesHoi <james352053767@gmail.com>
2020-02-22 16:30:30 +0800

    refactor: delete baiduAip class

--- speech/api.py
+++ speech/api.py
@@ -14,15 +14,14 @@ APP_ID = '17956963'
 API_KEY = 'y9umW9TaYizYaT6CyU08Bblv'
 SECRET_KEY = 'NMGN6AnQoYE357VKY6Q6e9VzV0sdACtO'
 
-class baiduAip:
-    def asr(self, audio):
-        client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
-        response = client.asr(audio, 'pcm', 16000, {
-            'dev_pid': DEV_PID.CN_ONLY.value,
-        })
-        return(response)
+def asr(audio):
+    client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+    response = client.asr(audio, 'pcm', 16000, {
+        'dev_pid': DEV_PID.CN_ONLY.value,
+    })
+    return(response)
 
-    def parser(self, text):
-        client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
-        response = client.depParser(text)
-        return(response)
+def parser(text):
+    client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
+    response = client.depParser(text)
+    return(response)
